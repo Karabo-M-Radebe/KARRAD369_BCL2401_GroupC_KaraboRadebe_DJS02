a97b8876cf81f554008ec315cc770db08e9da29f
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-05-10 06:05:16 +0200

    refactored the code and found a way to make it cleaner and readable

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const form = document.querySelector("[data-form]");
+/*const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
@@ -54,6 +54,41 @@ form.addEventListener("submit", (event) => {
   if (isNaN(dividend) || isNaN(divider)) {
     stringError();
   }
- });
+ });*/
 
 
+
+ // Refactored code
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+ 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+ 
+  function blankResult() {
+    result.innerText = 'Division not performed. Both values are required in inputs. Try again';
+  }
+ 
+  function zeroError() {
+    result.innerText = 'Division not performed. Invalid number provided. Try again';
+  }
+ 
+  function stringError() {
+    document.body.innerHTML = 'Something critical went wrong. Please reload the page';
+    console.error(new Error('Input is not a number'));
+  }
+ 
+  if (dividend === '' || divider === '') {
+    blankResult();
+  } else if (divider === '0') {
+    zeroError();
+    throw new Error ('Division by zero');
+  } else if (isNaN(dividend) || isNaN(divider)) {
+    stringError();
+  } else {
+    const wholeNumberResult = Math.floor(dividend/divider);
+    result.innerText = wholeNumberResult;
+  }
+});
